1e7e4670fefa3ffb19b22848216949cc0df2aab6
FArCaiBot <flavio397david@gmail.com>
2020-07-14 15:42:30 -0500

    test

--- src/main/java/com/jcse/ejemploAppRecetas/EjemploAppRecetasApplication.java
+++ src/main/java/com/jcse/ejemploAppRecetas/EjemploAppRecetasApplication.java
@@ -8,6 +8,7 @@ public class EjemploAppRecetasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EjemploAppRecetasApplication.class, args);
+		System.out.println("no hay ninguna receta, esta es una prueba");
 	}
 
 }
