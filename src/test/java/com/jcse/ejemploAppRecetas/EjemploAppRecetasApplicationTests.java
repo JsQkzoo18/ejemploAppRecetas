c7fca6ea138d70edfcd4ad977532b8528cd38566
John! <jhonmvl18@gmail.com>
2020-07-14 15:34:16 -0500

    Inicio de proyecto

---
+++ src/test/java/com/jcse/ejemploAppRecetas/EjemploAppRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcse.ejemploAppRecetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploAppRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
